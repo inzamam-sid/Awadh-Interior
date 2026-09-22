670458caaa7b9a1074f2803a40f95918f7ca30ae
inzamam-siddiqui <siddiquiinzmam319@gmail.com>
2026-09-22 20:23:54 +0530

    add lead routes: integrate leadRoutes into the main application for lead management

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./modules/auth/auth.routes.js";
 import errorMiddleware from "./middleware/error.middleware.js";
+import leadRoutes from "./modules/leads/lead.routes.js";
 
 const app = express();
 
@@ -20,6 +21,11 @@ app.get("/api/v1/health", (req, res) => {
 app.use(
   "/api/v1/auth",
   authRoutes
+);
+
+app.use(
+  "/api/v1/leads",
+  leadRoutes
 );
 
 app.use(errorMiddleware);
